d0290d3534abdacf1b2a2244c12b37a363121542
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2020-06-14 04:33:50 -0500

    refactor(Description): Refactor to BEM convention

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -3,19 +3,21 @@ import React, { Component } from "react";
 class Description extends Component {
   render() {
     return (
-      <section>
-        <h2 className="field-name">Description</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata
-        </p>
+      <section id="Description">
+        <div className="field">
+          <h2 className="field__name">Description</h2>
+          <p class="field__text">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata
+          </p>
+        </div>
       </section>
     );
   }
