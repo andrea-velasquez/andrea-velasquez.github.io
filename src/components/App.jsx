8ed32a7b5c4d03370b105a63c9e990e5f47bb89f
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2020-06-14 02:32:58 -0500

    feat(Description) Add Description component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 
 import Header from './Header/Header';
+import Description from './Description/Description';
 
 const App = () =>{
   return (
     <div className="paper">
       <Header/>
       <hr/>
+      <Description/>
     </div>
   );
 }
---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Description extends Component {
+  render() {
+    return (
+      <section>
+        <h2 className="field-name">Description</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata
+        </p>
+      </section>
+    );
+  }
+}
+
+export default Description;
