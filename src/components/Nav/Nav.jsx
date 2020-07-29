c78eb338a4d7e7002825e5c92ba45d9ca01cce93
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2020-07-29 00:34:25 -0500

    feat(Nav): Add Nav options and add hover transitions

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './Nav.scss';
+import { ReactComponent as SunIcon } from '../../assets/images/icons/sun.svg';
+import { ReactComponent as MoonIcon } from '../../assets/images/icons/moon.svg';
+import { ReactComponent as PdfIcon } from '../../assets/images/icons/pdf.svg';
 
 class Nav extends Component {
   state = {
@@ -10,17 +13,39 @@ class Nav extends Component {
       "Skills",
       "Experience",
       "Interests"
-    ]
-    
+    ],
+    currentTheme: "light"
   }
   render() {
     return (
       <nav>
-      {
-        this.state.sections.map( (section) =>
-          <a href={`#${section}`}>{section}</a>
-        )
-      }
+        <div className="nav__links">
+        {
+          this.state.sections.map( (section) =>
+            <a href={`#${section}`}>{section}</a>
+          )
+        }
+        </div>
+          <div className="nav__options">
+          <div className="nav__option" id="language">
+            <button>Es</button>
+            <span>Lang</span>
+          </div>
+          <div className="nav__option" id="theme">
+            <button>
+            { this.state.currentTheme === "light"
+              ? <MoonIcon/>
+              : <SunIcon/>
+            }
+            </button>
+            <span>Theme</span>
+          </div>
+          <br/>
+          <div className="nav__option" id="pdf">
+            <button><PdfIcon/></button>
+            <span>PDF here!</span>
+          </div>
+        </div>
       </nav>
     );
   }
