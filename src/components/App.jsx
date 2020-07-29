88082091f6a938c2fbe48d8d2d8203df2204d112
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2020-07-28 19:47:54 -0500

    feat(Nav): Add Nav component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,24 +9,28 @@ import Skills from './Skills/Skills';
 import Work from './Work/Work';
 import Interests from './Interests/Interests';
 import { ReactComponent as Logo } from '../assets/images/logo.svg';
+import Nav from "./Nav/Nav";
 
 const App = () =>{
   return (
-    <div className="paper">
-      <Header/>
-      <hr/>
-      <Description/>
-      <Education/>
-      <Projects/>
-      <Skills/>
-      <Work/>
-      <Interests/>
-      <div className="thanks">
-        <h2>Thanks for reading!</h2>
-        <Logo/>
+    <div>
+      <Nav />
+      <div className="paper">
+        <Header />
+        <hr />
+        <Description />
+        <Education />
+        <Projects />
+        <Skills />
+        <Work />
+        <Interests />
+        <div className="thanks">
+          <h2>Thanks for reading!</h2>
+          <Logo />
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './Nav.scss';
+
+class Nav extends Component {
+  state = {
+    sections: [
+      "Description",
+      "Education",
+      "Projects",
+      "Skills",
+      "Experience",
+      "Interests"
+    ]
+  }
+  render() {
+    return (
+      <nav>
+      {
+        this.state.sections.map( (section) =>
+          <a href={`#${section}`}>{section}</a>
+        )
+      }
+      </nav>
+    );
+  }
+}
+
+export default Nav;
