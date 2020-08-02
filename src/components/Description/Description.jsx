55d0fbaec147388ea4d7de11b5b7ce7b9a8d8dcd
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2020-08-02 02:21:33 -0500

    Render dynamic description

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
 
 class Description extends Component {
-  render() {
-    return (
-      <section id="Description">
-          <h2 className="field__name section__title">Description</h2>
-          <p className="field field__text">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+  state = {
+    description: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
             erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
             et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
@@ -14,7 +10,14 @@ class Description extends Component {
             sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
             et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
             accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-            no sea takimata
+            no sea takimata`
+  }
+  render() {
+    return (
+      <section id="Description">
+          <h2 className="field__name section__title">Description</h2>
+          <p className="field field__text">
+            { this.state.description }
           </p>
       </section>
     );
