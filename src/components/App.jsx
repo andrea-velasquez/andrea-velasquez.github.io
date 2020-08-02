ec2d7fc23fe91812c041cda30c133b68d29b1c0f
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2020-08-02 03:11:07 -0500

    Background: Change css for component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,14 @@ import Skills from './Skills/Skills';
 import Experience from './Experience/Experience';
 import Interests from './Interests/Interests';
 import Nav from "./Nav/Nav";
+import Background from "./Background/Background";
 
 import { ReactComponent as Logo } from '../assets/images/logo.svg';
 
 const App = () =>{
   return (
     <div id="app">
+      <Background />
       <Nav />
       <div className="paper">
         <Header />
