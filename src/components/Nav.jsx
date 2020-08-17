b7932287f40b9108e962064af871a443c2c69359
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2020-08-17 04:32:34 -0500

    feat(Nav): Responsive collapsing nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { ReactComponent as LeftArrow } from "../assets/images/icons/left-arrow.svg";
+import { ReactComponent as RightArrow } from "../assets/images/icons/right-arrow.svg";
 import { ReactComponent as SunIcon } from "../assets/images/icons/sun.svg";
 import { ReactComponent as MoonIcon } from "../assets/images/icons/moon.svg";
 import { ReactComponent as PdfIcon } from "../assets/images/icons/pdf.svg";
@@ -8,7 +10,35 @@ const capitalize = (w) => {
   return w;
 };
 
+const xlScreenWidth = 1200;
+
 class Nav extends Component {
+  state = {
+    collapsed: false
+  }
+  
+  onWindowResize = () => {
+    let collapsed = this.state;
+    if (xlScreenWidth > window.innerWidth && collapsed)
+      this.setState({collapsed: true});
+    else if (xlScreenWidth < window.innerWidth)
+      this.setState({collapsed: false});
+  }
+
+  toggleMenu = () => {
+    const collapsed = !this.state.collapsed;
+    this.setState({ collapsed });
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onWindowResize);
+    this.onWindowResize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onWindowResize);
+  }
+
   render() {
     const {
       constants,
@@ -20,11 +50,19 @@ class Nav extends Component {
 
     return (
       <nav id="nav">
-        <div className="nav__links">
-          {Object.keys(constants.sections).map((section, idx) => (
-            <a key={idx} href={`#${capitalize(section)}`}>{constants.sections[section]}</a>
-          ))}
+        <div className="nav__expand-button" onClick={this.toggleMenu}>
+          { this.state.collapsed
+            ? <LeftArrow/>
+            : <RightArrow/>
+          }
         </div>
+        { 
+          <div className={`nav__links ${this.state.collapsed? "collapsed" : ""}`}>
+            {Object.keys(constants.sections).map((section, idx) => (
+              <a key={idx} href={`#${capitalize(section)}`}>{constants.sections[section]}</a>
+            ))}
+          </div>
+        }
         <div className="nav__options">
           <div className="nav__option" id="language">
             <button onClick={onChangeLang}>{capitalize(otherLang)}</button>
