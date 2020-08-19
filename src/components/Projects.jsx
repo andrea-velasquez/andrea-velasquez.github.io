2caf5f1dc525385939825dc180463e7e658d1653
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2020-08-19 01:10:58 -0500

    Projects: Delete unused state

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,58 +20,6 @@ const svgLogos = reqToObject(reqLogosSvgs);
 const projectsImgs = reqToObject(reqProjectsImgs);
 
 class Projects extends Component {
-  state = {
-    projects: [
-      {
-        name: "Personal Website",
-        year: "2020",
-        link: "#",
-        technologies: ["react", "sass"],
-      },
-      {
-        name: "Yapay",
-        year: "2019",
-        link: "https://github.com/utec-yapay/yape-bcp-project-yapay",
-        technologies: [
-          "springboot",
-          "postgresql",
-          "vuejs",
-          "nuxtjs",
-          "flask",
-          "nativescript",
-        ],
-        collaboratorsGithub: [
-          "alessiayi",
-          "arturocuya",
-          "camilaferno",
-          "lilvedaes",
-          "Guillermo598",
-        ],
-      },
-      {
-        name: "Barranco Weather",
-        year: "2018",
-        link: "https://gitlab.com/barranco-weather",
-        technologies: ["flask", "php", "raspberrypi"],
-        collaboratorsGithub: ["arturocuya", "lilvedaes"],
-      },
-      {
-        name: "The CUAC Path",
-        year: "2017",
-        link: "https://github.com/andrea-velasquez/TheCuacPath/",
-        technologies: ["gamemaker"],
-        collaboratorsGithub: ["arturocuya", "lilvedaes"],
-      },
-      {
-        name: "Esnaque",
-        year: "2017",
-        link: "https://github.com/andrea-velasquez/esnaque/",
-        technologies: ["python"],
-        collaboratorsGithub: ["lilvedaes"],
-      },
-    ],
-  };
-
   getLogos = (technologies) => {
     let logos = technologies.reduce((logos, technology) => {
       logos[technology] = svgLogos[technology];
