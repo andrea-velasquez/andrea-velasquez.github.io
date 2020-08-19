31eeea16e0f8affdf362e68253c03cd4fb0fc780
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2020-08-19 04:46:25 -0500

    Updates

---
+++ precache-manifest.cd990920166476c634ef874b5c36e12b.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a8a12581eda4167766835b79d10efc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c0de11f3fadc4d6886a",
+    "url": "/static/css/main.3a7534b8.chunk.css"
+  },
+  {
+    "revision": "6d26636dc03741c9ef12",
+    "url": "/static/js/2.25bce3a3.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/static/js/2.25bce3a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c0de11f3fadc4d6886a",
+    "url": "/static/js/main.4913d006.chunk.js"
+  },
+  {
+    "revision": "4ac598fa20d7619937c8",
+    "url": "/static/js/runtime-main.90834230.js"
+  },
+  {
+    "revision": "68fa8cbf6357ae208f51015413e24aeb",
+    "url": "/static/media/angular.68fa8cbf.svg"
+  },
+  {
+    "revision": "22d816e7a325ab4dbf0810605ab9f17f",
+    "url": "/static/media/barranco-weather.22d816e7.png"
+  },
+  {
+    "revision": "ec79d1210f9ea471c7bdd9578daec10d",
+    "url": "/static/media/c++.ec79d121.svg"
+  },
+  {
+    "revision": "88bc12541a65dd264a597e7a76e63317",
+    "url": "/static/media/c.88bc1254.svg"
+  },
+  {
+    "revision": "58aa0443d99006a550efc388f941f9ae",
+    "url": "/static/media/clip.58aa0443.svg"
+  },
+  {
+    "revision": "d43dd8e1f8dd16cefdfbe3d51f10248f",
+    "url": "/static/media/css.d43dd8e1.svg"
+  },
+  {
+    "revision": "59964c8b8e9fc7cff10fc48dd021c227",
+    "url": "/static/media/django.59964c8b.svg"
+  },
+  {
+    "revision": "4d9d252943cf5443f9c2dbb5288a04d9",
+    "url": "/static/media/email.4d9d2529.svg"
+  },
+  {
+    "revision": "a76b83815b82d794c9a88bb9fa858cd9",
+    "url": "/static/media/esnaque.a76b8381.png"
+  },
+  {
+    "revision": "2e551eccebe0f53f455e61266fcb61d5",
+    "url": "/static/media/flask.2e551ecc.svg"
+  },
+  {
+    "revision": "5bdd43bff6924b7354154027a5896c3c",
+    "url": "/static/media/folder.5bdd43bf.svg"
+  },
+  {
+    "revision": "3c6806528456dd030563ce71937c4141",
+    "url": "/static/media/game-maker.3c680652.svg"
+  },
+  {
+    "revision": "269f0b116f213276ff261e238769c289",
+    "url": "/static/media/github.269f0b11.svg"
+  },
+  {
+    "revision": "5c208b91e74f494ae7a10c07e380ff2c",
+    "url": "/static/media/go.5c208b91.svg"
+  },
+  {
+    "revision": "266ac49b822d88f9705369d8d72aca3f",
+    "url": "/static/media/half-typewriter.266ac49b.svg"
+  },
+  {
+    "revision": "42dce94e40204b479b643acbf143ec67",
+    "url": "/static/media/html-css.42dce94e.svg"
+  },
+  {
+    "revision": "bf773e4fb46a10035b401b6d9a9a3390",
+    "url": "/static/media/html.bf773e4f.svg"
+  },
+  {
+    "revision": "719e3e39f093797fbf7946694dd201b4",
+    "url": "/static/media/java.719e3e39.svg"
+  },
+  {
+    "revision": "2de955fc6f1360c55f65b83c83616ec4",
+    "url": "/static/media/jrhand.2de955fc.ttf"
+  },
+  {
+    "revision": "d72e80771e070d75b496ad378c0b514d",
+    "url": "/static/media/left-arrow.d72e8077.svg"
+  },
+  {
+    "revision": "28be8b18e8d439019c3011d3fc19d9e8",
+    "url": "/static/media/linkedin.28be8b18.svg"
+  },
+  {
+    "revision": "8ceb46c155f03f58af813a94db43cbdc",
+    "url": "/static/media/logo.8ceb46c1.svg"
+  },
+  {
+    "revision": "f06d6bbd2e5e7a48acffbdb8f8353989",
+    "url": "/static/media/me.f06d6bbd.jpg"
+  },
+  {
+    "revision": "cad4d011429b351db9451f042a11c9c6",
+    "url": "/static/media/mongodb.cad4d011.svg"
+  },
+  {
+    "revision": "a6f24b11212c5a9fd6bb78ce830e68a6",
+    "url": "/static/media/moon.a6f24b11.svg"
+  },
+  {
+    "revision": "a6a4b061ed7cd848b3e07c09ff47c955",
+    "url": "/static/media/nativescript.a6a4b061.svg"
+  },
+  {
+    "revision": "5126c6db2573c32c8ba1e8a8156b2286",
+    "url": "/static/media/nodejs.5126c6db.svg"
+  },
+  {
+    "revision": "815f8a63164148fcc19e2721d734ec15",
+    "url": "/static/media/nuxtjs.815f8a63.svg"
+  },
+  {
+    "revision": "efff293d2d2463ca72f52ad509d96da4",
+    "url": "/static/media/pdf.efff293d.svg"
+  },
+  {
+    "revision": "d919fb7d8210825143c101a3404ff614",
+    "url": "/static/media/pen.d919fb7d.svg"
+  },
+  {
+    "revision": "2be24525d370e9482328732dc27ef435",
+    "url": "/static/media/personal-website.2be24525.png"
+  },
+  {
+    "revision": "5b4a2f360627a417cf4e300e8ade20ce",
+    "url": "/static/media/php.5b4a2f36.svg"
+  },
+  {
+    "revision": "ff00c390356205e25230f59b9f68dbf0",
+    "url": "/static/media/postgresql.ff00c390.svg"
+  },
+  {
+    "revision": "9f6d70d536b472fcaffba0c0078684a4",
+    "url": "/static/media/python.9f6d70d5.svg"
+  },
+  {
+    "revision": "0a3fc953f35dfe3c8d4b7af2f34f0add",
+    "url": "/static/media/r.0a3fc953.svg"
+  },
+  {
+    "revision": "a822dc05328935b4f34a0932ceebfba2",
+    "url": "/static/media/raspberry-pi.a822dc05.svg"
+  },
+  {
+    "revision": "5615bdff6efaf1450d74e132470eba97",
+    "url": "/static/media/react.5615bdff.svg"
+  },
+  {
+    "revision": "6699b5dbaa4c590a77d2a7684d6cff48",
+    "url": "/static/media/resume-en.6699b5db.pdf"
+  },
+  {
+    "revision": "fdf98a5bc458bd49f035e220f813ef43",
+    "url": "/static/media/resume-es.fdf98a5b.pdf"
+  },
+  {
+    "revision": "f6f7eaa7bdcebe9ae7d4675f320fe029",
+    "url": "/static/media/right-arrow.f6f7eaa7.svg"
+  },
+  {
+    "revision": "73bdc040ed3cc5e21f208a78048896f0",
+    "url": "/static/media/sass.73bdc040.svg"
+  },
+  {
+    "revision": "7d3ff09bbe04470ba3881c5adb1ef00d",
+    "url": "/static/media/springboot.7d3ff09b.svg"
+  },
+  {
+    "revision": "f4f1b4064d23ca9db4e9cffac043d46f",
+    "url": "/static/media/sql.f4f1b406.svg"
+  },
+  {
+    "revision": "f0bedf2db531fc4e359b46592803d4d2",
+    "url": "/static/media/sun.f0bedf2d.svg"
+  },
+  {
+    "revision": "6c41610a2eb1ca617bd0dfbeaee22885",
+    "url": "/static/media/tape.6c41610a.svg"
+  },
+  {
+    "revision": "27020899cf9d5ef053d5e03b9b1a77f4",
+    "url": "/static/media/the-cuac-path.27020899.png"
+  },
+  {
+    "revision": "471a9d2997146b30e4a78e1efb0e0f22",
+    "url": "/static/media/utec.471a9d29.jpg"
+  },
+  {
+    "revision": "80d0d3bfd0bf4ce7a04694167b6ad67f",
+    "url": "/static/media/veraType.80d0d3bf.ttf"
+  },
+  {
+    "revision": "1243c82b6e9ac7e7dca72cbc3049c2d3",
+    "url": "/static/media/vuejs.1243c82b.svg"
+  },
+  {
+    "revision": "1faa6f679abf6e56b7d68d1fff9f8ecd",
+    "url": "/static/media/wolfram.1faa6f67.svg"
+  },
+  {
+    "revision": "5c0054d6540b3fd0583a490acbc1e7d3",
+    "url": "/static/media/yapay.5c0054d6.png"
+  }
+]);
